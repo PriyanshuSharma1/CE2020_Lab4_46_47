43379800eee637b64f8fccef6e8d8a96dbf4a035
PriyanshuSharma1 <psneb420@gmail.com>
2022-11-07 14:33:39 +0545

    Interface class created

---
+++ AbstractBST.h
@@ -0,0 +1,12 @@
+#ifndef AbstractBST_h
+#define AbstractBST_h
+class AbstractBST
+{
+public:
+    virtual void isEmpty();
+    virtual void addBST();
+    virtual void removeBST();
+    virtual void searchBST();
+};
+
+#endif
